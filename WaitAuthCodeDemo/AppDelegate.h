011d9251eb5bd72fefd22340f9cea5d2d8768d2e
lYcHeeM <luo_zhijun@163.com>
2014-11-18 18:14:16 +0800

    Initial Commit

---
+++ WaitAuthCodeDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WaitAuthCodeDemo
+//
+//  Created by luozhijun on 14-11-18.
+//  Copyright (c) 2014年 bgyun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WaitAuthCodeDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WaitAuthCodeDemo
+//
+//  Created by luozhijun on 14-11-18.
+//  Copyright (c) 2014年 bgyun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
